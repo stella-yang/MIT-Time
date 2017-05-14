13aba86c9d6401b02a519f4e11d847ae7cb115b4
Stella Yang <stella.lan.yang@gmail.com>
2017-05-14 15:37:18 -0400

    heroku port

--- server.js
+++ server.js
@@ -27,10 +27,11 @@ mongoose.connect("mongodb://stellay:stellay@ds155080.mlab.com:55080/heroku_wrnw9
 
 var db = mongoose.connection;
 
+var port = process.env.PORT || 3000;
 
 var app = express()
-app.listen(3000, function () {
-  console.log('Listening on port 3000!')
+app.listen(port, function () {
+  console.log('Listening on port ' + port)
 })
 
 // Connect Flash
